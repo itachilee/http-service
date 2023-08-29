d2ac1ca5b7be03275a2217a2bddc0ba4bf263fcd
itachilee <3050155685@qq.com>
2023-08-29 23:45:59 +0800

    Stream & TcpListener

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    println!("Listening on port 3000!");
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+        println!("Connection established");
+
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
+    }
 }
